a636e79fe2c8402feb9e670f2046822be01af6a8
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2023-04-24 21:46:40 +0300

    added initial styling for the main page

--- src/components/Common/Footer/Footer.tsx
+++ src/components/Common/Footer/Footer.tsx
@@ -1,17 +1,26 @@
+import './Footer.css'
+
 function Footer() {
   return (
-    <footer className="footer">
+    <footer className="footer container">
       <p className="footer__copyright">
         Учебный проект Яндекс.Практикум x BeatFilm.
       </p>
       <div className="footer__row">
         <div className="footer__timestamp">&copy; 2023</div>
         <ul className="footer__links">
-          <li className="footer__link">
-            <a href="https://practicum.yandex.ru/">Яндекс.Практикум</a>
+          <li className="footer__links-item">
+            <a className="footer__link" href="https://practicum.yandex.ru/">
+              Яндекс.Практикум
+            </a>
           </li>
-          <li className="footer__link">
-            <a href="https://github.com/vladimir-andrushchenko2">Github</a>
+          <li className="footer__links-item">
+            <a
+              className="footer__link"
+              href="https://github.com/vladimir-andrushchenko2"
+            >
+              Github
+            </a>
           </li>
         </ul>
       </div>
