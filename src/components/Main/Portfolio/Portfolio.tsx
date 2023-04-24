2e20d085d8754d27694f34b3ba905e06595b8dae
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2023-04-24 21:24:44 +0300

    styled portfolio section

--- src/components/Main/Portfolio/Portfolio.tsx
+++ src/components/Main/Portfolio/Portfolio.tsx
@@ -1,20 +1,30 @@
+import './Portfolio.css'
+
 function Portfolio() {
   return (
-    <section className="portfolio">
-      <h2 className="portfolio__title main-page__section-title">Портфолио</h2>
+    <section className="portfolio container">
+      <h2 className="portfolio__title">Портфолио</h2>
       <ul className="portfolio__links">
-        <li className="portfololio__link">
-          <a href="https://github.com/vladimir-andrushchenko2/how-to-learn">
+        <li className="portfolio__link-item">
+          <a
+            className="portfolio__link"
+            href="https://github.com/vladimir-andrushchenko2/how-to-learn"
+          >
             Статичный сайт
           </a>
         </li>
-        <li className="portfololio__link">
-          <a href="https://github.com/vladimir-andrushchenko2/country-travel">
+        <li className="portfolio__link-item">
+          <a
+            className="portfolio__link"
+            href="https://github.com/vladimir-andrushchenko2/country-travel"
+          >
             Адаптивный сайт
           </a>
         </li>
-        <li className="portfololio__link">
-          <a href="https://vova.today">Одностраничное приложение</a>
+        <li className="portfolio__link-item">
+          <a className="portfolio__link" href="https://vova.today">
+            Одностраничное приложение
+          </a>
         </li>
       </ul>
     </section>
