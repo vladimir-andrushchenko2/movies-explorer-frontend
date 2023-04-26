f9c823c5a37c288e8509e8c18f8ae6e5a2b82a3d
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2023-04-26 23:08:51 +0300

    refactored MoviesCardList

--- src/components/MoviesList/MoviesCardList/MoviesCardList.tsx
+++ src/components/MoviesList/MoviesCardList/MoviesCardList.tsx
@@ -1,9 +1,8 @@
 import MovieCard from '../MoviesCard/MoviesCard'
 import './MoviesCardList.css'
 import CircularCheckbox from '../../UiElements/CircularCheckbox/CircularCheckbox'
-import { useMemo } from 'react'
 
-type Card = 'save' | 'delete'
+type CardType = 'save' | 'delete'
 
 type Movie = {
   id: number
@@ -13,10 +12,10 @@ type Movie = {
 type MoviesCardListProps = {
   onClickHandler: (id: number) => void
   movies: Movie[]
-  cardType: Card
+  cardType: CardType
 }
 
-const typeToButton = {
+const cardTypeToButtonJSXElementFactory = {
   save: ({
     isLiked,
     onClickHandler,
@@ -34,38 +33,28 @@ const typeToButton = {
   ),
 }
 
-function makeCards<T extends keyof typeof typeToButton>(
-  cardType: T,
-  movies: Movie[],
-  onClickHandler: (id: number) => void,
-) {
-  const makeButton = typeToButton[cardType]
-
-  return movies.map((movie) => {
-    return (
-      <MovieCard
-        key={movie.id}
-        actionButton={makeButton({
-          ...movie,
-          onClickHandler: () => onClickHandler(movie.id),
-        })}
-      />
-    )
-  })
-}
-
 function MoviesCardList({
   onClickHandler,
   movies,
   cardType,
 }: MoviesCardListProps) {
-  const cards = useMemo(() => {
-    return makeCards(cardType, movies, onClickHandler)
-  }, [cardType, movies, onClickHandler])
+  const makeButton = cardTypeToButtonJSXElementFactory[cardType]
 
   return (
     <section className="movies-list">
-      <ul className="movies-list__list">{cards}</ul>
+      <ul className="movies-list__list">
+        {movies.map((movie) => {
+          return (
+            <MovieCard
+              key={movie.id}
+              actionButton={makeButton({
+                ...movie,
+                onClickHandler: () => onClickHandler(movie.id),
+              })}
+            />
+          )
+        })}
+      </ul>
     </section>
   )
 }
