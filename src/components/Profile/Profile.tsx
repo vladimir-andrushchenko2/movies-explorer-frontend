4a3fef242fe02a8aae642595ea38fe76bf7a684f
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2023-04-27 20:38:02 +0300

    initial styling for the profile page

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,5 +1,43 @@
+import Header from '../Common/Header/Header'
+import './Profile.css'
+
 function Profile() {
-  return <h1>Profile</h1>
+  return (
+    <div className="profile-page container">
+      <Header />
+      <div className="profile">
+        <h1 className="profile__title">Привет, Виталий!</h1>
+        <form id="profile__form">
+          <label className="profile__label">
+            <span className="profile__label-text">Имя</span>
+            <input
+              className="profile__input"
+              type="text"
+              placeholder={'Виталий'}
+            />
+          </label>
+          <label className="profile__label">
+            <span className="profile__label-text">E-mail</span>
+            <input
+              className="profile__input"
+              type="email"
+              placeholder={'test@test.com'}
+            />
+          </label>
+        </form>
+
+        <div className="profile__buttons">
+          <input
+            className="profile__edit-btn"
+            type="submit"
+            value={'Редактировать'}
+            form="profile__form"
+          />
+          <button className="profile__logout">Выйти из аккаунта</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Profile
